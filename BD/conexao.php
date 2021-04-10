d31a2dcc1e2a12ed84c6daaede99a497f2640518
Talitha Souza <startechlds@gmail.com>
2021-04-10 14:00:42 -0300

    criação da lógica de login e alteração do titulo pagina inical para index

---
+++ BD/conexao.php
@@ -0,0 +1,25 @@
+<?php
+class ConexaoBD{
+ 
+    public function ConDB(){
+
+        try{
+            DEFINE('HOST','localhost');
+            DEFINE('BD','gerenciamento_estagio');
+            DEFINE('USER','root');
+            DEFINE('SENHA','');
+            
+            $conect = new PDO('mysql:host='.HOST.';dbname='.BD,USER,SENHA);
+            $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+           // echo("CONECTADO COM SUCESSO");
+            
+            return $conect;
+        }
+        catch(PDOException $e){
+            echo("ERRO<br>");
+            echo($e->getMessage());
+        }
+    } 
+}
+?>
---
+++ index.php
@@ -0,0 +1,110 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/loginInicial/style.css">
+    <title>Login</title>
+</head>
+
+ 
+
+<body>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-6 col-sm-12" style="display: flex; justify-content: center; margin-top: 190px;">
+
+                <h2>Sistema de Gerência de Estágios (UVA)</h2>
+
+            </div>
+
+        </div>
+
+        <form class="form-row" method="POST" action="">
+
+            <div class="col form-group" style="display: flex; align-items: center; justify-content: center; margin-top: 10px">
+
+                <input type="email" id="usuario" name="usuario" placeholder="Usuário" class="form-control formInicial"/>
+
+            </div>
+        
+            <div class="col form-group" style="display: flex; align-items: center; justify-content: center;">
+
+                <input type="password" id="senha" name="senha" placeholder="Senha" required class="form-control formInicial"/>
+
+            </div>
+
+            <div class="row">
+
+                <div class="col" style="display: flex; align-items: center; justify-content: center;">
+
+                    <button class="btn btn-primary" name="btn_login" type="submit" >Entrar</button>
+
+                </div>
+
+            </div>
+        </form>
+
+        <?php
+            include_once('BD/conexao.php');
+
+            if(isset($_POST['btn_login'])){
+                $usuario = filter_input(INPUT_POST,'usuario', FILTER_DEFAULT);
+                $senha = filter_input(INPUT_POST,'senha', FILTER_DEFAULT);
+                $conn = new ConexaoBD();
+                $conect = $conn->ConDB();
+
+                $select = "SELECT * FROM pessoa WHERE CH_Usuario=:usuarioLogin AND CH_Senha=:senhaLogin";
+
+
+                try{
+                    $resultLogin = $conect->prepare($select);
+                    $resultLogin->bindParam(':usuarioLogin',$usuario, PDO::PARAM_STR);
+                    $resultLogin->bindParam(':senhaLogin',$senha, PDO::PARAM_STR);
+                    $resultLogin->execute();
+
+                    $verificarRetorno = $resultLogin->rowCount();
+
+                    if($verificarRetorno > 0){
+                        $arr = $resultLogin->fetch(PDO::FETCH_ASSOC);
+                        $tipoUsuario = $arr['VF_Tipo'];
+
+                        $_SESSION['loginUser'] = $_POST['usuario'];
+                        $_SESSION['loginSenha'] = $_POST['senha'];
+                        $_SESSION['tipoUsuario'] = $tipoUsuario;
+                        if($tipoUsuario == 'A'){
+                            header("Location: inicialAlunoVagas.html");
+                        }
+                        else if ($tipoUsuario == 'C' or $tipoUsuario == 'P'){
+                            header("Location: BemVindoCoordenador.html");
+                        }
+                        
+                    }
+                    else{
+                        echo '<div class="alert alingn alert-danger"><button type="button" class="close" data-dismiss="alert">x</button>
+                        <strong>Erro!</strong> usuário ou senha incorretos, digite outro login ou contate o professor responsável pela disciplina de estágio :(</div>';
+                    }
+                    
+                    
+                }
+                catch(PDOException $e){
+                    echo('<br><br>');
+                    echo("ERRRO<br>");
+                    echo($e->getMessage());
+                    //echo("<br>".$email." ".$senha);
+                }
+
+            }
+        ?>
+        
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
